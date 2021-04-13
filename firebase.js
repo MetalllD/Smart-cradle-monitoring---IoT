4a8fb40db2938f724de81dc84f90fa33bddb6a23
mitalkamani <45828315+MetalllD@users.noreply.github.com>
2021-04-14 02:50:27 +0530

    Connected data fields to firebase

--- firebase.js
+++ firebase.js
@@ -1,13 +1,13 @@
-//referencing DOM 
+////  referencing DOM  /////////////////////////////////////////////////////////////////////////
 var tempValue = document.getElementById('tempValue');
 var cryingState = document.getElementById('cryingState');
 var humidValue = document.getElementById('humidValue');
 var fan = document.getElementById('fan');
 var swinging = document.getElementById('swinging');
 var toy = document.getElementById('toy');
-
-console.log(fan.state);
-
+var fanPrev = 0;
+var swingPrev = 0;
+var toyPrev = 0;
 /*Email.send({
     Host : "smtp.gmail.com",
     Username : "mitalkamani500@gmail.com",
@@ -26,25 +26,126 @@ function randomNumber(min, max){
     return Math.floor(r)
 }
 
-//function to generate noise and detect crying
-var noise = randomNumber(60, 120);
-console.log(noise);
+//function to detect crying
+var noiseRandom ,tempRandom, humidRandom = 0;
 function detectCrying(int){
-    if(int>90)
+    if(int>90){
     return "Yes"
+    }
+    else{
+    return "No"
+    }
 }
-
-
 ////      SENDIND PART  //////////////////////////////////////////////////////////////////////////////////////
 //referencing database
 dbsend = firebase.database().ref();
-
+dbsend.set({fan:0,swinging:0})
 //sending values to database
-dbsend.set({
-    temperature:randomNumber(20, 35),
-    humidity:randomNumber(65, 100),
-    crying:detectCrying(noise)
-})
+function sendData(){
+
+    noise = randomNumber(60, 120); 
+    tempRandom = randomNumber(20, 35);
+    humidRandom = randomNumber(65, 100);
+
+    dbsend.set({
+        toy:toyPrev,
+        swinging:swingPrev,
+        fan:fanPrev,
+        temperature:tempRandom,
+        humidity: humidRandom,
+        crying:detectCrying(noise) 
+    })
+
+    fan.addEventListener('change', function(event) {
+        if(event.target.checked){
+        console.log("Fan On");
+          fanPrev = 1;
+          dbsend.set({
+              fan:1,
+              toy:toyPrev,
+              swinging:swingPrev,
+              temperature:tempRandom,
+              humidity: humidRandom,
+              crying:detectCrying(noise) 
+          });
+        }
+        else{
+          fanPrev = 0;
+          dbsend.set({
+              fan:0,
+              toy:toyPrev,
+              swinging:swingPrev,
+              temperature:tempRandom,
+              humidity: humidRandom,
+              crying:detectCrying(noise) 
+          });
+          console.log("Fan Off");
+        }
+      });
+
+      swinging.addEventListener('change', function(event) {
+        if(event.target.checked){
+        console.log("Swinging On");
+          swingPrev = 1;
+          dbsend.set({
+              swinging:1,
+              fan:fanPrev,
+              toy:toyPrev,
+              temperature:tempRandom,
+              humidity: humidRandom,
+              crying:detectCrying(noise) 
+          });
+        }
+        else{
+          swingPrev = 0;
+          dbsend.set({
+              swinging:0,
+              fan:fanPrev,
+              toy:toyPrev,
+              temperature:tempRandom,
+              humidity: humidRandom,
+              crying:detectCrying(noise) 
+          });
+          console.log("Swinging Off");
+        }
+      });
+
+
+      toy.addEventListener('change', function(event) {
+        if(event.target.checked){
+        console.log("toy On");
+          toyPrev = 1;
+          dbsend.set({
+              toy:1,
+              fan:fanPrev,
+              swinging:swingPrev,
+              temperature:tempRandom,
+              humidity: humidRandom,
+              crying:detectCrying(noise) 
+          });
+        }
+        else{
+          toyPrev = 0;
+          dbsend.set({
+              toy:0,
+              fan:fanPrev,
+              swinging:swingPrev,
+              temperature:tempRandom,
+              humidity: humidRandom,
+              crying:detectCrying(noise) 
+          });
+          console.log("toy Off");
+        }
+      });
+      console.log(noise);
+}
+   
+
+
+setInterval(sendData,5000);
+
+
+
 
 
 ////      DISPLAYING PART  //////////////////////////////////////////////////////////////////////////////////////
@@ -59,3 +160,25 @@ dbhumid.on('value',snap => humidValue.innerText = snap.val());
 //displaying crying values to webapp
 var dbcrying = firebase.database().ref('crying/');
 dbcrying.on('value',snap => cryingState.innerText = snap.val());
+
+var fanState = firebase.database().ref('/fan');                  
+fanState.on("value", function (snap) {
+            if(snap.val()==1){
+        }
+    })
+
+var swingState = firebase.database().ref('/swinging');                  
+swingState.on("value", function (snap) {
+            if(snap.val()==1){
+        }
+    })
+
+
+var toyState = firebase.database().ref('/toy');                  
+toyState.on("value", function (snap) {
+                if(snap.val()==1){
+            }
+        })
+    
+
+////// Toggles //////////////////////////////////////////////////////////////////////////////////////////////////
